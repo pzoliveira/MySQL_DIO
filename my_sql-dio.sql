f682565a7f2a8ed69e813ebaf67e0bf091fedee1
Pericles Zapata de Oliveira <pzoliveira@outlook.com>
2022-03-29 21:57:57 -0300

    JOIN part 3

--- my_sql-dio.sql
+++ my_sql-dio.sql
@@ -161,3 +161,5 @@ SELECT * FROM videos_canais JOIN videos ON videos_canais.fk_video = videos.id_vi
 SELECT * FROM videos_canais AS vc JOIN videos AS v ON vc.fk_video = v.id_video;
 SELECT * FROM videos_canais AS vc JOIN videos AS v ON vc.fk_video = v.id_video
 JOIN canais AS c ON vc.fk_canal = c.id_canal;
+SELECT v.nome_video, v.autor_video, c.nome_canal FROM videos_canais AS vc JOIN videos AS v ON vc.fk_video = v.id_video
+JOIN canais AS c ON vc.fk_canal = c.id_canal;
